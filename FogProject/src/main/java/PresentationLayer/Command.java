a35cb271ab37d1e14c5ab6dfa19d204c779c2d7b
Jbakke <Jbakke@Jonatan-desktop.bakke.net>
2017-11-16 16:15:06 +0100

    Added Command and UnknownCommand

---
+++ FogProject/src/main/java/PresentationLayer/Command.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PresentationLayer;
+
+import FunctionLayer.FogProjectException;
+import java.util.HashMap;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Jbakke
+ */
+abstract class Command {
+    private static HashMap<String, Command> commands;
+
+    private static void initCommands() {
+        commands = new HashMap<>();
+    }
+
+    static Command from( HttpServletRequest request ) {
+        String commandName = request.getParameter( "command" );
+        
+        System.out.println(commandName);
+        if ( commands == null ) {
+            initCommands();
+        }
+        return commands.getOrDefault(commandName, new UnknownCommand() );
+    }
+
+    abstract String execute( HttpServletRequest request, HttpServletResponse response ) throws FogProjectException;
+}
---
+++ FogProject/src/main/java/PresentationLayer/UnknownCommand.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PresentationLayer;
+
+import FunctionLayer.FogProjectException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Jbakke
+ */
+public class UnknownCommand extends Command{
+
+    @Override
+    String execute(HttpServletRequest request, HttpServletResponse response) throws FogProjectException {
+        String msg = "Unknown command. Contact IT";
+        throw new FogProjectException(msg );
+    }
+    
+}
