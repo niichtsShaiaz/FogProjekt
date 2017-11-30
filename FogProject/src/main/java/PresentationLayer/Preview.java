ba5b4e84b5b8bd8fd8d43a47b7ef74dfc15c5b71
niichtsShaiaz <mathias@igel.it>
2017-11-30 10:44:54 +0100

    weo weo

--- FogProject/src/main/java/PresentationLayer/Preview.java
+++ FogProject/src/main/java/PresentationLayer/Preview.java
@@ -19,7 +19,30 @@ public class Preview {
          sb.append (poles2(width, length));
          sb.append (outerLines(width, length));
          sb.append (flatRoof(width, length));
+         sb.append (rem(width, length));
        
+        return sb.toString();
+    }
+    public static String rem(int width, int length){
+        int space = 0;
+        StringBuilder sb = new StringBuilder();
+        
+        int j = (width) / 300 - 1;
+        
+        if (j != 0)
+            space = width/(j+1);
+        else 
+            space = width;
+        
+        for (int i = 300; i<width; i+=300)
+        {
+            sb.append( "<rect x='"+(length-length-10)+"' y='"+(space)+"' height='"+5+"' width='"+(length+12)+"' style='stroke:#000000; fill:#ff0000 '/>");
+            space += space;
+        }
+        sb.append( "<rect x='"+(length-length-10)+"' y='"+(width-width+70)+"' height='"+5+"' width='"+(length+12)+"' style='stroke:#000000; fill:#ff0000 '/>");
+        sb.append( "<rect x='"+(length-length-10)+"' y='"+(width-70)+"' height='"+5+"' width='"+(length+12)+"' style='stroke:#000000; fill:#ff0000 '/>");
+        System.out.println("j = "+j);
+        System.out.println("space = " + space);
         return sb.toString();
     }
     public static String flatRoof(int width, int length){
@@ -27,7 +50,7 @@ public class Preview {
         
         
         for (int i = 55; i < length; i+=55) {
-            sb.append( "<rect x='"+(i)+"' y='"+(0)+"' height='"+width+"' width='"+2+"' style='stroke:#000000; fill: #000000'/>");
+            sb.append( "<rect x='"+(i)+"' y='"+(0)+"' height='"+width+"' width='"+2+"' style='stroke:#000000; fill: #8b4513'/>");
             
         }
         
@@ -50,10 +73,10 @@ public class Preview {
         
         StringBuilder sb = new StringBuilder();
 
-        sb.append( "<rect x='"+(length-length+10)+"' y='"+(width-width+10)+"' height='"+sizeY+"' width='"+sizeX+"' style='stroke:#000000; fill: #000000'/>");
-        sb.append( "<rect x='"+(length-10)+"' y='"+(width-width+10)+"' height='"+sizeY+"' width='"+sizeX+"' style='stroke:#000000; fill: #000000'/>");
-        sb.append( "<rect x='"+(length-length+10)+"' y='"+(width-10)+"' height='"+sizeY+"' width='"+sizeX+"' style='stroke:#000000; fill: #000000'/>");
-        sb.append( "<rect x='"+(length-10)+"' y='"+(width-10)+"' height='"+sizeY+"' width='"+sizeX+"' style='stroke:#000000; fill: #000000'/>");
+        sb.append( "<rect x='"+(length-length+10)+"' y='"+(width-width+70)+"' height='"+sizeY+"' width='"+sizeX+"' style='stroke:#000000; fill: #000000'/>");
+        sb.append( "<rect x='"+(length-10)+"' y='"+(width-width+70)+"' height='"+sizeY+"' width='"+sizeX+"' style='stroke:#000000; fill: #000000'/>");
+        sb.append( "<rect x='"+(length-length+10)+"' y='"+(width-70)+"' height='"+sizeY+"' width='"+sizeX+"' style='stroke:#000000; fill: #000000'/>");
+        sb.append( "<rect x='"+(length-10)+"' y='"+(width-70)+"' height='"+sizeY+"' width='"+sizeX+"' style='stroke:#000000; fill: #000000'/>");
         
         return sb.toString();
     }
@@ -64,8 +87,8 @@ public class Preview {
         
         for (int i = 275; i<length; i+=275)
         {
-            sb.append( "<rect x='"+(i)+"' y='"+(width-width+10)+"' height='"+sizeY+"' width='"+sizeX+"' style='stroke:#000000; fill: #000000'/>");
-            sb.append( "<rect x='"+(i)+"' y='"+(width-10)+"' height='"+sizeY+"' width='"+sizeX+"' style='stroke:#000000; fill: #000000'/>");
+            sb.append( "<rect x='"+(i)+"' y='"+(width-width+70)+"' height='"+sizeY+"' width='"+sizeX+"' style='stroke:#000000; fill: #000000'/>");
+            sb.append( "<rect x='"+(i)+"' y='"+(width-70)+"' height='"+sizeY+"' width='"+sizeX+"' style='stroke:#000000; fill: #000000'/>");
         }
         
         return sb.toString();
@@ -85,8 +108,8 @@ public class Preview {
         
         for (int i = 300; i<width; i+=300)
         {
-            sb.append( "<rect x='"+(length-10)+"' y='"+(space-10)+"' height='"+sizeY+"' width='"+sizeX+"' style='stroke:#000000; fill: #000000'/>");
-            sb.append( "<rect x='"+(length-length+10)+"' y='"+(space-10)+"' height='"+sizeY+"' width='"+sizeX+"' style='stroke:#000000; fill: #000000'/>");
+            sb.append( "<rect x='"+(length-10)+"' y='"+(space)+"' height='"+sizeY+"' width='"+sizeX+"' style='stroke:#000000; fill: #000000'/>");
+            sb.append( "<rect x='"+(length-length+10)+"' y='"+(space)+"' height='"+sizeY+"' width='"+sizeX+"' style='stroke:#000000; fill: #000000'/>");
             space += space;
         }
         System.out.println("j = "+j);
