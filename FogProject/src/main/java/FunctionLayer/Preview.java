7344d1b60eee0ac9925f54c2d26041779e4351ed
EZL1190 <e.z.l@live.com>
2017-12-01 12:03:57 +0100

    rettede tegning

--- FogProject/src/main/java/FunctionLayer/Preview.java
+++ FogProject/src/main/java/FunctionLayer/Preview.java
@@ -16,10 +16,10 @@ public class Preview {
          
          sb.append (cornerPoles(width, length));
          sb.append (poles(width, length));
-         sb.append (poles2(width, length));
+         //sb.append (poles2(width, length)); //error når bredde er <= 300
          sb.append (outerLines(width, length));
          sb.append (flatRoof(width, length));
-         sb.append (rem(width, length));
+         //sb.append (rem(width, length)); //error når bredde er <= 300
        
         return sb.toString();
     }
